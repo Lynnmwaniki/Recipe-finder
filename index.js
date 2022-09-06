89cf4876bf0608e11e26999084e7aa4cb423d13a
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2022-09-06 23:08:30 +0300

    added fetch function to fetch data and images from API

--- index.js
+++ index.js
@@ -9,3 +9,33 @@ mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
+
+//get the list of meals that match with the ingredients
+function getMealList(){
+    let searchInputText = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}`)
+    .then(resp => resp.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal =>{
+                html += `
+                <div class = "meal-item" data-id = ${meal.idMeal}">
+                    <div class = "meal-img">
+                        <img src = "${meal.strMealThumb}" alt = "food">
+                    </div>
+                    <div class = "meal-name">
+                        <h3>"${meal.strMeal}"</h3>
+                        <a href = "#" class = "recipe-btn">Get Recipe</a>
+                    </div>
+                </div>
+            `;
+            });
+            mealList.classList.remove('notFound')
+        } else{
+            html = "OOPs! We couldn't find any meal";
+            mealList.classList.add('notFound');
+        }
+        mealList.innerHTML = html;
+    });
+}
