80d46b0d0e172444468effcdbec9a2e86554183c
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2022-09-14 11:52:58 +0300

    final code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
-const mealDetailsContent = document.querySelector('meal-details-content');
+const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 // adding event listeners
@@ -10,32 +10,34 @@ recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
+
 //get the list of meals that match with the ingredients
 function getMealList(){
-    let searchInputText = document.getElementById('search-input').value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}`)
-    .then(resp => resp.json())
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
     .then(data => {
         let html = "";
         if(data.meals){
-            data.meals.forEach(meal =>{
+            data.meals.forEach(meal => { 
                 html += `
-                <div class = "meal-item" data-id = ${meal.idMeal}">
-                    <div class = "meal-img">
-                        <img src = "${meal.strMealThumb}" alt = "food">
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
                     </div>
-                    <div class = "meal-name">
-                        <h3>"${meal.strMeal}"</h3>
-                        <a href = "#" class = "recipe-btn">Get Recipe</a>
-                    </div>
-                </div>
-            `;
+                `;
             });
-            mealList.classList.remove('notFound')
+            mealList.classList.remove('notFound');
         } else{
-            html = "OOPs! We couldn't find any meal";
+            html = "Oops, we didn't find any meal!";
             mealList.classList.add('notFound');
         }
+
         mealList.innerHTML = html;
     });
 }
@@ -44,12 +46,13 @@ function getMealList(){
 function getMealRecipe(e){
     e.preventDefault();
     if(e.target.classList.contains('recipe-btn')){
-    let mealItem = e.target.parentElement.parentElement;
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-    .then(resp => resp.json())
-    .then(data => mealRecipeModal(data.meals));
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
 }
-}
+
 // modal creation(A modal window creates a mode that disables the main window but keeps it visible, with the modal window as a child window in front of it.)
 function mealRecipeModal(meal){
     console.log(meal);
@@ -64,10 +67,17 @@ function mealRecipeModal(meal){
         <div class = "recipe-meal-img">
             <img src = "${meal.strMealThumb}" alt = "">
         </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
+        
     `;
     mealDetailsContent.innerHTML = html;
-    mealDetailsContent.parentElement.classList.add('showRecipe'); 
+    mealDetailsContent.parentElement.classList.add('showRecipe');
 }
+
+//sending message function
+$('form#contactForm').submit(function (event) {
+    event.preventDefault();
+    const name = $('#name').val();
+    const pass = $('#email').val();
+    const mess = $('#mess').val();
+    alert("Hello " + name + "....Thank you we have received your message and we will respond to you as soon as possible. Thank you for contacting us.");
+});
